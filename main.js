9b2d9855696de8e8db42ccc264bfa3e46981d580
Yuki Kaneko <kanekoyukibs@gmail.com>
2021-03-28 20:24:39 +0900

    コメントアウト

--- main.js
+++ main.js
@@ -52,15 +52,16 @@
 
 
 
-const addButton = document.getElementById('add')
-const removeButton = document.getElementById('remove')
-const list = document.querySelector('.list') // ※既に書いている場合は省略
+// const addButton = document.getElementById('add')
+// const removeButton = document.getElementById('remove')
+// const list = document.querySelector('.list') // ※既に書いている場合は省略
 
-addButton.addEventListener('click', (event) => { // <button id="add">がクリックされたとき…
-  const newItem = document.createElement('li')
-  newItem.textContent = '新しいアイテム'
-  list.appendChild(newItem)
-})
-removeButton.addEventListener('click', (event) => { // <button id="remove">がクリックされたとき…
-  list.removeChild(list.lastElementChild)
-})
+// addButton.addEventListener('click', (event) => { // <button id="add">がクリックされたとき…
+//   const newItem = document.createElement('li')
+//   newItem.textContent = '新しいアイテム'
+//   list.appendChild(newItem)
+// })
+// removeButton.addEventListener('click', (event) => { // <button id="remove">がクリックされたとき…
+//   list.removeChild(list.lastElementChild)
+// })
+
